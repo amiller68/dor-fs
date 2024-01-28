ea043110d634781d3aa9d45f6c1ecbff790915ff
Al Miller <alexanderscottmiller@gmail.com>
2024-01-28 18:46:19 -0500

    fix: bullet points

--- bin/test/writing/hello_world.md
+++ bin/test/writing/hello_world.md
@@ -8,3 +8,8 @@ This is an example of linking to a image within the store we set up.
     If you want links within the markdown to work, you need to use the relative path to the file. and prefix it with a dot and a slash. 
 -->
 <img src="./../visual/petting_turtles.jpg" alt="turtles" width="400"/>
+
+This is an example of using bullet points:
+- This is a bullet point
+- This is another bullet point
+- This is a third bullet point
--- src/wasm/utils.rs
+++ src/wasm/utils.rs
@@ -21,6 +21,10 @@ pub fn markdown_to_html(content: String) -> String {
     let mut options = Options::empty();
     options.insert(Options::ENABLE_STRIKETHROUGH);
     options.insert(Options::ENABLE_TASKLISTS);
+    options.insert(Options::ENABLE_TABLES);
+    options.insert(Options::ENABLE_FOOTNOTES);
+    options.insert(Options::ENABLE_SMART_PUNCTUATION);
+    options.insert(Options::ENABLE_TASKLISTS);
 
     let parser = Parser::new_ext(&content, options);
     let mut html = String::new();
