4df32d845c1aa3f9a222d70557f0672aa4dbd8d4
Al Miller <alexanderscottmiller@gmail.com>
2024-01-28 19:00:12 -0500

    fix: somehow included bad build

--- src/wasm/env.rs
+++ src/wasm/env.rs
@@ -4,7 +4,7 @@
 
 pub const APP_VERSION: &str = "2.0.0";
 pub const APP_NAME: &str = "Krondor.Org";
-pub const APP_CONTRACT_ADDRESS: &str = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-pub const APP_CHAIN_ID: &str = "31337";
-pub const APP_RPC_URL: &str = "http://localhost:8545";
-pub const APP_IPFS_GATEWAY_URL: &str = "http://localhost:8080";
+pub const APP_CONTRACT_ADDRESS: &str = "0x8740bb2ded8C1aaABbBcbB6f7087B9cEE944B31D";
+pub const APP_CHAIN_ID: &str = "11155111";
+pub const APP_RPC_URL: &str = "https://sepolia.infura.io/v3/c2a72953873d451f81839acff0f498cd";
+pub const APP_IPFS_GATEWAY_URL: &str = "https://krondor.infura-ipfs.io";
